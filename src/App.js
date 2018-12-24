8a12de0d309f9e7dcba7fa2266a4c8945a166335
prnvshrn <prnvshrn@gmail.com>
2018-12-24 01:54:12 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './components/input.js';
 
 class App extends Component {
   render() {
+    const url = 'https://api.edamam.com/search?q=chicken&app_id=5c4f25a2&app_key=d5ca218c0a52e2d593a48aca92c0e4e3&from=0&to=3&calories=591-722&health=alcohol-free';
+
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data.hits);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Input/>
       </div>
     );
   }
---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Input extends Component{
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default Input;
